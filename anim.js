494e14550f1865e8ca4aba56dd3618b07b518aa2
Edgar-Rios <71159256+Edgar-Rios@users.noreply.github.com>
2026-09-21 19:55:06 -0300

    Update lyrics data in anim.js

--- anim.js
+++ anim.js
@@ -5,23 +5,23 @@ var boton = document.querySelector('.boton');
 // audio.autoplay = true
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: 'Tus ojitos son las joyas mas lindas', time: 7 },
-  { text: 'Sos la musica que suena en el aire cuando las flores comienzan a bailar', time: 14 },
-  { text: 'Tus ojos brillan como sol en cielo despejado', time: 21 },
-  { text: 'Si las flores son lindas, vos superas la naturaleza', time: 28 },
-  { text: 'Hoy celebro tu esfuerzo como estudiante y la belleza de la primavera que llevas con vos', time: 35 },
-  { text: 'Cada flor que veo me recuerda a vos', time: 42 },
+  { text: 'Asi como hay distintas flores, hay distintas personas', time: 7 },
+  { text: 'Cada una con su brillo, aroma, delizadeza y belleza particular', time: 14 },
+  { text: 'Y aun asi de todas las que hay sos la mas especial', time: 21 },
+  { text: 'De esta forma el mundo parece un jardin gigante', time: 28 },
+  { text: 'Pero ninguna se compara con el brillo de tus ojos... ', time: 33 },
+  { text: 'Ni con el encanto de tu sonrisa ', time: 38 },
+  { text: 'Sos esa luz que resalta de manera natural, sin buscarlo', time: 42 },
+  { text: 'luz brillante y calma casi angelical', time: 47 },
   { text: 'Es primavera, y el sol brilla, pero vos lo haces mucho mas', time: 49 },
-  { text: "Hoy abriste los ojos y el sol guardo su pincel", time: 56 },
-  { text: "Es que pintas el paisaje mejor que el", time: 63 },
-  { text: 'Como el sol con las flores, sos el sol que ilumina mis dias', time: 70 },
-  { text: 'Tu amor es el brote que florecio en mi marchitado corazon', time: 77 },
-  { text: 'Cada día que pasa mi amor por vos florece mas', time: 84 },
-  { text: 'La distancia no puede marchitar el amor que siento por vos', time: 91 },
+  { text: "En tu forma de ser encuentro todo...", time: 56 },
+  { text: "Todo lo que vale la pena admirar", time: 63 },
+  { text: 'Esta cancion es la que resuena en mi mente cada vez que te pienso', time: 70 },
+  { text: 'Es una pregunta que se responde solo cuando uno te ve', time: 77 },
+  { text: 'Porque todos quieren estar cerca tuyo? No hay porque... no hay pregunta', time: 84 },
+  { text: 'Es porque sos como sos, tan perfecta que asusta', time: 91 },
   // { text: 'En este jardin falta la flor mas bella. Vos', time:  },
-  { text: 'Te pido perdon bebu. Como la primavera renace quiero que nuestro amor florezca de nuevo', time: 98 },
-  { text: "ailoviu turra rolinga gotica😁.", time: 105 },
-    
+  { text: "Feliz primavera preciosa flor del jardin del paraiso", time: 96 },    
 ];
 
 // Animar las letras
